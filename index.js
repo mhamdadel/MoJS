395d0c4c49a9215759272b00a521c2ec410b6ef2
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-06-11 14:26:04 +0300

    add exports

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-const Encapsulation = require('./lib/encapsulation');
-const encapsulation = new Encapsulation({
-    name: 'Muhammed Adel',
-    age: 30,
-  }, {
-    name: [
-      function(value) {
-        return value.toUpperCase();
-      },
-      function(value) {
-        return value.trim();
-      },
-    ],
-    age: function(value) {
-      if (value < 0) {
-        throw new Error('age cannot be negative');
-      }
-      return value;
-    },
-  }, {
-    age: function(value) {
-      return value + ' years old';
-    },
-  });
-  const encapsulatedObj = encapsulation.encapsulate();
-  console.log(encapsulatedObj.getName());
-  console.log(encapsulatedObj.getAge()); 
-  encapsulatedObj.setName('   Muhammed   ');
-  encapsulatedObj.setAge(35);
-  console.log(encapsulatedObj.getName());
-  console.log(encapsulatedObj.getAge());
+module.exports = {
+    Encapsulation : require('./lib/encapsulation'),
+    freeze: require('./lib/freeze'),
+    unFreeze: require('./lib/unFreeze'),
+}
